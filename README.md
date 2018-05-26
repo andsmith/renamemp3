8a6c07bb649f474993bfed6260451fc94cb164db
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2018-05-26 15:21:31 -0700

    Rename mp3 files from a UUID to human readable derived from MP3 tags.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # renamemp3
+
+Rename mp3 files from a UUID (common to many podcast hosting sites) to something human-readable. E.g.:
+
+    `09c2efd1-9b80-4696-a412-59d1d51522bc.mp3`
+
+becomes:
+
+    `ID10T with Chris Hardwick - Natalie Dormer.mp3`
+
+or, with the artist's name abbreviated:
+
+    `IwCH - Natalie Dormer.mp3`
+
+
+### Syntax
+
+In the directory containing the MP3 files, run:
+
+    python renamemp3.py
+
+Use the option `-f` or `--full_artist` to use the entire artist string.
+
+This scans the CWD for files matching the format <UUID>.mp3, reads the mp3 tag strings and suggests an output filename for each file found.
+
+The files are not renamed, but the commands to rename them are printed, so you just have to copy & paste them to the command line.
+
+#### Requirements
+python
+
+`mp3_tagger` python module
---
+++ renamemp3.py
@@ -0,0 +1,50 @@
+import mp3_tagger
+import os
+import sys
+import re
+
+
+def get_new_filename(old_filename, do_full_artist=False, quiet=True):
+    m = mp3_tagger.MP3File(old_filename)
+    tags = m.get_tags()
+    artist = tags['ID3TagV2']['artist'].encode('utf-8')
+    song = tags['ID3TagV2']['song'].encode('utf-8')
+    track = tags['ID3TagV1']['track']  # not sure what to do with this...
+    if do_full_artist:
+        artist_name = artist
+    else:
+        # use artist's initials, keep case
+        artist_name = "".join([a for i, a in enumerate(artist) if (i == 0 or artist[i - 1] == ' ')])
+    # strip out bad chars
+    song_clean = "".join([a for a in song if ((a >= 'A' and a <= 'Z') or (a >= 'a' and a <= 'z') or (a == ' ') or (a >= '0' and a <= '9'))])
+    out_file = "%s - %s.mp3" % (artist_name, song_clean)
+    if not quiet:
+        print "\t%s %s %s" % (old_filename, ("%s" % (track, )).ljust(6), out_file)
+    return out_file
+
+
+if __name__ == "__main__":
+    do_full = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] in ['-f', '--full_artist']:
+            do_full = True
+        else:
+            print "Unknown command."
+            print "Syntax:  python renamemp3.py [-f|--full-artist]"
+            sys.exit()
+
+    files = [f for f in os.listdir('.') if f.lower().endswith('mp3')]
+    matching = [f for f in files if re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}.mp3", f)]
+    renames = []
+
+    print "\nScanning...\n"
+    print "\tMatching MP3 found                       Track  Rename to"
+    for c in matching:
+        out_file = get_new_filename(c, do_full_artist=do_full, quiet=False)
+        cmd = 'mv "%s" "%s"' % (c, out_file)
+        renames.append(cmd)
+    print "\nPaste these commands to terminal to rename mp3 files:\n"
+
+    for c in renames:
+        print c
+    print "\n"
