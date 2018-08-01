57261e1af4864116f82e38fb6485f7e38c6aa563
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2018-08-01 09:20:26 -0700

    Files starting with UUID also, remove trailing spaces.

--- renamemp3.py
+++ renamemp3.py
@@ -7,6 +7,7 @@ import re
 def get_new_filename(old_filename, do_full_artist=False, quiet=True):
     m = mp3_tagger.MP3File(old_filename)
     tags = m.get_tags()
+
     artist = tags['ID3TagV2']['artist'].encode('utf-8')
     song = tags['ID3TagV2']['song'].encode('utf-8')
     track = tags['ID3TagV1']['track']  # not sure what to do with this...
@@ -17,6 +18,7 @@ def get_new_filename(old_filename, do_full_artist=False, quiet=True):
         artist_name = "".join([a for i, a in enumerate(artist) if (i == 0 or artist[i - 1] == ' ')])
     # strip out bad chars
     song_clean = "".join([a for a in song if ((a >= 'A' and a <= 'Z') or (a >= 'a' and a <= 'z') or (a == ' ') or (a >= '0' and a <= '9'))])
+    song_clean = re.sub(' *$', '', song_clean)
     out_file = "%s - %s.mp3" % (artist_name, song_clean)
     if not quiet:
         print "\t%s %s %s" % (old_filename, ("%s" % (track, )).ljust(6), out_file)
@@ -34,7 +36,7 @@ if __name__ == "__main__":
             sys.exit()
 
     files = [f for f in os.listdir('.') if f.lower().endswith('mp3')]
-    matching = [f for f in files if re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}.mp3", f)]
+    matching = [f for f in files if re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}.*\.mp3", f)]
     renames = []
 
     print "\nScanning...\n"
